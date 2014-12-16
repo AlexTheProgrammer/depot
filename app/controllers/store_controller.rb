c2c2de81cb70d6d4763ef932348e97384b2575ec
Alexander Streeter <alexander.business.s@gmail.com>
2014-12-16 23:59:30 +1030

    Included Ajax for shopping cart.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -8,7 +8,15 @@
 #---
 class StoreController <
 ApplicationController
+  include CurrentCart
+  before_action :set_cart
+
   def index
    @products = Product.order(:title)
+   if session[:counter].nil?
+	session[:counter] = 0
+   else
+	session[:counter] += 1
+   end
   end
 end
